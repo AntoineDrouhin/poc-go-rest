5f0e6f14d3d416a1b3068a4fb8f091095b6344e5
Antoine Drouhin <antoine.drouhin@gmail.com>
2018-02-09 11:42:24 +0100

    add multiple routes

--- server.go
+++ server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 
@@ -12,11 +11,34 @@ import (
 func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
+
 	router.HandleFunc("/", Index)
+	router.HandleFunc("/help", Index)
+
+	router.HandleFunc("/todos", TodoIndex)
+	router.HandleFunc("/todos/{todoId}", TodoShow)
+
+	log.Print("Server running on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
 
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	fmt.Fprintf(w,
+		`API usage Guide : 
+			/help
+			/helloWorld
+			/todos
+			/todos/{todoId}
+		`)
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Todo Index !")
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoId := vars["todoId"]
+	fmt.Fprintf(w, "Todo ID : %q ! ", todoId)
 }
