7b471c7e09d69c4aa3831bf42f58333314269010
Antoine Drouhin <antoine.drouhin@gmail.com>
2018-02-10 11:22:05 +0100

    Refactor / Split in 4 files

---
+++ handler.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w,
+		`API usage Guide : 
+			/help
+			/helloWorld
+			/todos
+			/todos/{todoId}
+		`)
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	todos := Todos{
+		Todo{Name: "Write presentation"},
+		Todo{Name: "Host meetup"},
+	}
+
+	if err := json.NewEncoder(w).Encode(todos); err != nil {
+		panic(err)
+	}
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoId := vars["todoId"]
+	fmt.Fprintf(w, "Todo ID : %q ! ", todoId)
+}
--- main.go
+++ main.go
@@ -1,48 +1,15 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
-	router := mux.NewRouter().StrictSlash(true)
 
-	router.HandleFunc("/", Index)
-	router.HandleFunc("/help", Index)
-
-	router.HandleFunc("/todo", TodoIndex)
-	router.HandleFunc("/todo/{todoId}", TodoShow)
+	router := NewRouter()
 
 	log.Print("Server running on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
-}
 
-func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w,
-		`API usage Guide : 
-			/help
-			/helloWorld
-			/todos
-			/todos/{todoId}
-		`)
-}
-
-func TodoIndex(w http.ResponseWriter, r *http.Request) {
-	todos := Todos{
-		Todo{Name: "Write presentation"},
-		Todo{Name: "Host meetup"},
-	}
-
-	json.NewEncoder(w).Encode(todos)
-}
-
-func TodoShow(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	todoId := vars["todoId"]
-	fmt.Fprintf(w, "Todo ID : %q ! ", todoId)
 }
